d4f25085ce625cfebced3d6d1f26cceed8e0aac2
Andris Krauze <andris.krauze@ksdigital.lv>
2021-01-11 09:37:42 +0200

    Changed: drop the date validation to support after-2017 codes starting with "32" Added: example index.html page

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # id-lv
-Latvijas personas koda kontrolcipara un datuma daļas pārbaudes Javascript valodā.
-Latvian person identification code checksum and date validator in Javascript.
+Latvijas personas koda kontrolcipara pārbaudes Javascript valodā.
+Latvian person identification code checksum validator in Javascript.
 
 # Lietošanas piemēri / Examples of usage
 Iekļaujam id-lv.js faila saturu savā projektā un gatavs lietošanai.
 Include id-lv.js file in your project and you are ready to go.
 
 ```javascript
-if (IdeLve.isValidIdentificationCode("111111-11111")) {
+let validator = new IdeLve();
+
+if (validator.isValidIdentificationCode("111111-11111")) {
   console.log("jā, būs pareizs kods");
   console.log("ok, valid code");
 }
-if (IdeLve.isValidIdentificationCode("111112-11111")) {
+if (validator.isValidIdentificationCode("111112-11111")) {
   console.log("nē, nebūs pareizs kods");
   console.log("invalid code");
 }
--- id-lv.js
+++ id-lv.js
@@ -1,85 +1,55 @@
-var IdeLve  = {
-	/**
-	 * Validates if string is a valid date against a pattern dd.mm.yyyy
-	 * @param dateString
-	 * @returns {boolean}
-	 */
-	isValidDate: function (dateString) {
-		// First check for the pattern
-		if (!/^\d{1,2}\.\d{1,2}\.\d{4}$/.test(dateString))
-			return false;
-		// Parse the date parts to integers
-		var parts = dateString.split(".");
-		var day = parseInt(parts[0], 10);
-		var month = parseInt(parts[1], 10);
-		var year = parseInt(parts[2], 10);
-		// Check the ranges of month and year
-		if (year < 1000 || year > 3000 || month === 0 || month > 12)
-			return false;
-		var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-		// Adjust for leap years
-		if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0))
-			monthLength[1] = 29;
-		// Check the range of the day
-		return day > 0 && day <= monthLength[month - 1];
-	},
-
+function IdeLve () {
 	/**
 	 * Validates personal identification code issued by Republic of Latvia
 	 * @param identificationCode string - Identification code.
 	 * @returns error codes if identificationCode is not valid.
 	 */
-	validateIdentificationCode: function (identificationCode) {
-		var pattern;
-		var errorCodes = [];
-		// check basic pattern (123456-78901)
-		pattern = new RegExp("([0-2]\\d|[3][0-1])([0]\\d|[1][0-2])(\\d{2})-([0-2])(\\d{3})(\\d)");
+	this.validateIdentificationCode = function (identificationCode) {
+		let errorCodes = [];
 
-		var match = pattern.exec(identificationCode);
+		if (identificationCode.length > 12) {
+			errorCodes.push("too-long");
+			return errorCodes;
+		}
+
+		// check basic pattern (123456-78901)
+		let pattern = new RegExp("(\\d{6})-(\\d{4})(\\d)");
+
+		let match = pattern.exec(identificationCode);
 		if (match === null) {
 			errorCodes.push("invalid-code");
 			return errorCodes;
 		}
 
-		//get the century
-		var century = "";
-		century = ((+match[4]) === 1 ? "19" : (+match[4]) === 2 ? "20" : "18");
-
-		var inputDay = (+match[1]);
-		var inputMonth = (+match[2]);
-		var inputYear = century + (match[3]);
-		if (!IdeLve.isValidDate(inputDay + "." +inputMonth + "." + inputYear)) {
-			errorCodes.push("invalid-code-not-valid-date");
-			return errorCodes;
-		}
-
 		// check checksum digit
-		var identificationCodeWithoutDash = match[0].replace("-", "");
-		var checkSumDigits = [1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
-		var checkSum = 0;
+		let identificationCodeWithoutDash = match[0].replace("-", "");
+		let checkSumDigits = [1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+		let checkSum = 0;
+		let checkDigit = +match[3];
 
-		for (var index in checkSumDigits) {
+		for (let index in checkSumDigits) {
 			if (checkSumDigits.hasOwnProperty(index)) {
-				checkSum += ((+identificationCodeWithoutDash.charAt(index)) * (+checkSumDigits[index]));
+				checkSum += ((+identificationCodeWithoutDash.charAt(+index)) * (+checkSumDigits[+index]));
 			}
 		}
 		checkSum = (1 - checkSum) % 11;
 		checkSum += (checkSum < -1) ? 11 : 0;
 
 		//checksum should match last digit
-		if (checkSum !== +match[6]) {
+		if (checkSum !== checkDigit) {
 			errorCodes.push("invalid-code-bad-checksum");
 		}
+
 		return errorCodes;
-	},
+	}
 
 	/**
 	 * Check if identification code is valid
 	 * @param identificationCode
 	 * @returns {boolean}
 	 */
-  	isValidIdentificationCode: function (identificationCode) {
-		return ! IdeLve.validateIdentificationCode(identificationCode).length > 0;
+	this.isValidIdentificationCode = function (identificationCode) {
+		return !this.validateIdentificationCode(identificationCode).length > 0;
 	}
 
-};
+}
